c74309f5638227052bac7d6a3f10b0285cfdf271
rr <101080386+ranrango@users.noreply.github.com>
2024-11-22 22:43:12 +0800

    20241122
    111

---
+++ contents/Skills.md
@@ -0,0 +1,3 @@
+
+
+- [[docker部署]](https://github.com/ranrango/ranrango.github.io/tree/main/contents/Skills/docker部署.html)
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,7 +2,7 @@
 
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['Home', 'CourseNotes', 'PaperNotes']
+const section_names = ['Home', 'CourseNotes', 'PaperNotes', 'Skills']
 
 
 window.addEventListener('DOMContentLoaded', event => {
